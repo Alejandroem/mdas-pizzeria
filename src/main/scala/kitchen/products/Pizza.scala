7c59b9f40bb080b6a1ed22ffdc65ba1017a4d873
Alejandro <alexalejandroem@gmail.com>
2019-06-26 20:16:39 +0200

    Added getters

--- src/main/scala/kitchen/products/Pizza.scala
+++ src/main/scala/kitchen/products/Pizza.scala
@@ -5,5 +5,10 @@ import scala.collection.mutable.ArrayBuffer
 class Pizza {
   private var _size = null
   private var _type = null
-  private val _ingredients: ArrayBuffer[Ingredients]
+  private val _ingredients: ArrayBuffer[Ingredient] = ArrayBuffer[Ingredient]()
+
+  def getSize = _size
+  def getType = _type
+  def getIngredients = _ingredients
+
 }
