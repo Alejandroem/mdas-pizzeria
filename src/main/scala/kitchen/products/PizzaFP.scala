2ae40f93145687ee9ff55ed60abf72e8764f5610
Alejandro <alexalejandroem@gmail.com>
2019-06-26 21:22:57 +0200

    add remove and remove all ingredients

--- src/main/scala/kitchen/products/PizzaFP.scala
+++ src/main/scala/kitchen/products/PizzaFP.scala
@@ -4,6 +4,16 @@ import kitchen.characteristics.{Ingredient, Size, Type}
 
 case class PizzaFP (t:Type, s:Size, i:Seq[Ingredient]){
 
-  def addIngredient(i:Ingredient)
+  def addIngredient(ingredient:Ingredient): PizzaFP = {
+    this.copy(i = this.i:+ ingredient)
+  }
+
+
+
+  def removeIngredient(i: Ingredient) = ???
+
+  def removeAllIngredients():PizzaFP = {
+    this.copy(i = Seq[Ingredient]())
+  }
 
 }
