08d5546209d80c027cd98df593bab137f56d8130
Alejandro <alexalejandroem@gmail.com>
2019-06-26 20:32:14 +0200

    added crust type

---
+++ src/main/scala/kitchen/characteristics/Type.scala
@@ -0,0 +1,8 @@
+package kitchen.characteristics
+
+trait Type
+
+case object Original extends Type
+case object Normal extends Type
+case object Thick extends Type
+
--- src/main/scala/kitchen/products/Pizza.scala
+++ src/main/scala/kitchen/products/Pizza.scala
@@ -1,6 +1,6 @@
 package kitchen.products
 
-import kitchen.characteristics.Ingredient
+import kitchen.characteristics.{Ingredient, Medium, Normal, Size, Type}
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -8,13 +8,22 @@ class Pizza {
 
   type Coste = BigDecimal
 
-  private var _size = null
-  private var _type = null
+  private var _size = Medium
+  private var _type:Type = Normal
   private val _ingredients: ArrayBuffer[Ingredient] = ArrayBuffer[Ingredient]()
 
   def getSize = _size
   def getType = _type
   def getIngredients = _ingredients
+
+  def setSize(s: Size): Unit ={
+    _size = s
+  }
+
+  def setType(t:Type): Unit ={
+    _type = t
+  }
+
 
   def addIngredient(i:Ingredient) = {
     _ingredients += i
